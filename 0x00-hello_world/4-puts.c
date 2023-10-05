3eb9a51cb1d84ca27da8e500e74d465e28eab96f
HALLAL <hmdhallal@gmail.com>
2023-10-05 09:23:48 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: always 0 (Succes)
- *     */
+ * main - Starting point
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
 	puts ("\"Programming is like building a multilingual puzzle");
