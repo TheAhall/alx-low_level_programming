c1fb34bf644471b09f495d64e4313367ca5e73ec
HALLAL <hmdhallal@gmail.com>
2023-10-06 12:23:44 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,14 +8,14 @@
 int main(void)
 {
 	int c, i, n;
-	
+
 	for (c = 0; c < 8; c++)
 	{
-		for (i =1; i < 10; i++)
+		for (i = 1; i < 10; i++)
 		{
 			for (n = 2; n < 10; n++)
 			{
-				if (i < n && c < i )
+				if (i < n && c < i)
 				{
 					putchar('0' + c);
 					putchar('0' + i);
