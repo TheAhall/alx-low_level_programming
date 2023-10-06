d3979ce4afc59bac98f930cdcc508ccce4f5920f
HALLAL <hmdhallal@gmail.com>
2023-10-06 09:09:00 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * mai - Entery point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char i, n, c;
+
+        for (i = 'a'; i < 'e'; i++)
+		putchar(i);
+	for (n = 'f'; n < 'q'; c++)
+		putchar(i);
+	for (c = 'r'; c <= 'z'; c++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
