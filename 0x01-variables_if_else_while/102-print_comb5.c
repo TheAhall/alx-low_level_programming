1cc0377f329ba0c2ebc2f8073143a6e0910e5db1
HALLAL <hmdhallal@gmail.com>
2023-10-06 12:40:53 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i, n, a, b;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (n = 0; n < 9; n++)
+		{
+			for (a = 0; a < 9; a++)
+			{
+				for (b = 1; b < 10; b++)
+				{
+					if (b > n && a >= i)
+					{
+						putchar('0' + i);
+						putchar('0' + n);
+						putchar('0' + a);
+						putchar('0' + b);
+						if (a != 8)
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
