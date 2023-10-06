adbde69f5e561e87903b565866aeec33aa1be585
HALLAL <hmdhallal@gmail.com>
2023-10-06 08:57:33 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *  * main - Entery point
+ *   *
+ *    * Return: Always 0
+ *     */
+int main(void)
+{
+	char i, n;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	putchar('\n');
+	for (n = 'A'; n <= 'Z'; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
