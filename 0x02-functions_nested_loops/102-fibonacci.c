59e4165c92196830c8ef838206b6bead925380bc
HALLAL <hmdhallal@gmail.com>
2023-10-10 12:59:36 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,17 +10,17 @@ int main(void)
 	int i = 0;
 	long j = 1, k = 2;
 
-	while
+	while (i < 58)
 	{
 		if (i == 0)
-			printf("%d", j);
+			printf("%ld", j);
 		else if (i == 1)
-			printf(", %d", k);
+			printf(", %ld", k);
 		else
 		{
 			k += j;
 			j = k - j;
-			printf(", %d", k);
+			printf(", %ld", k);
 		}
 		++i;
 	}
