c0f9ff84401f2c5a2d2e0d06993beaff3a5e185e
HALLAL <hmdhallal@gmail.com>
2023-10-12 09:37:55 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_diagonal - satrt point
+ * @n: variable
+ * Return: value
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j <= i; j++)
+				_putchar(' ');
+		}
+		_putchar('\\');
+	}
+	_putchar('\n');
+}
