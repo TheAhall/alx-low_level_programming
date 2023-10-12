d0ba0ec09f50c44a5a7836123eb3415f816909de
HALLAL <hmdhallal@gmail.com>
2023-10-12 12:25:34 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - start point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i, j, n;
+
+	for (i = 1; i <= 100; i++)
+	{
+		j = i % 3;
+		n = i % 5;
+		if (j != 0  && n != 0)
+			printf("%d ", i);
+		else
+		{	
+			if (j == 0 && j != n)
+				printf("Fizz ");
+			if (n == 0 && n != j)
+				printf("Buzz ");
+			if (j == 0 && n == j)
+				printf("FizzBuzz ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
