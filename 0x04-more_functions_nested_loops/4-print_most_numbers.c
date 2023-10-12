5c1b24eeccb88ce7cafd2afc95f5409d634a5ddb
HALLAL <hmdhallal@gmail.com>
2023-10-12 08:07:28 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - function to print numbers
+ *
+ * Return: 0
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+			_putchar('0' + i);
+	}
+	_putchar('\n');
+}
