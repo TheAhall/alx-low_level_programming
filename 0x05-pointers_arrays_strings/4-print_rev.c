828f8787803f4efaa308ac8259e001a43e71242f
HALLAL <hmdhallal@gmail.com>
2023-10-16 09:52:44 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - function to print srting in reverse 
+ * @s: Variable to be printed in reverse
+ * Return: value
+ */
+void print_rev(char *s)
+{
+	int i;
+	int j = strlen(s) -1;
+
+	for (i = j; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
