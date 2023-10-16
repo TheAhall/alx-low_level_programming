d90e29049b6c10d225439aeed729efcc86d50400
HALLAL <hmdhallal@gmail.com>
2023-10-16 09:54:44 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,14 +2,14 @@
 #include <string.h>
 
 /**
- * print_rev - function to print srting in reverse 
+ * print_rev - Function to print srting in reverse 
  * @s: Variable to be printed in reverse
  * Return: value
  */
 void print_rev(char *s)
 {
 	int i;
-	int j = strlen(s) -1;
+	int j = strlen(s) - 1;
 
 	for (i = j; i >= 0; i--)
 		_putchar(s[i]);
