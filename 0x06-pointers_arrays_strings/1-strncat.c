3623ad8c9970112ef3ab9bb11572793dcf6b2325
HALLAL <hmdhallal@gmail.com>
2023-10-18 07:01:08 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,11 +2,11 @@
 #include <string.h>
 
 /**
- * _strncat - Function that concatenates two strings.
+ * _strncat - Function that concatenates two strings
  * @dest: variable 1
  * @src: variable 2
  * @n: variable 3
- * return: 0
+ * Return: 0
  */
 char *_strncat(char *dest, char *src, int n)
 {
