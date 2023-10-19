777abd6d5fd9d277f48771103d6abe176ac32fcb
HALLAL <hmdhallal@gmail.com>
2023-10-19 08:16:09 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
 
-/**
- * main - Entery point
- *
- * Return: Always 0
- */
 int main(void)
 {
 	int n;
@@ -21,7 +16,7 @@ int main(void)
 	 * - only one statement
 	 * - you are not allowed to code anything else than this line of code
 	 */
-	*(&n + 5) = 98;
+	int k; *(&k + 5) = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
