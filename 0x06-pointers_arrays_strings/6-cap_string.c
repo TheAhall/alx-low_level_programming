f4ee10d40d152f21ca805b6e22b4bb8f88c55cc0
HALLAL <hmdhallal@gmail.com>
2023-10-19 06:04:11 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,12 +8,21 @@
  */
 char *cap_string(char *a)
 {
-	int i;
+	int i, j;
+	char p[13] = {' ', '\n', '\t', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
 
 	for (i = 0; a[i] != '\0'; i++)
 	{
-		if (i == 0 || a[i - 1] == ' ')
+		if (i == 0 && a[i] >= 'a' && a[i] <= 'z')
 			a[i] = toupper(a[i]);
+		for (j = 0; j < 13; j++)
+		{
+			if (a[i] == p[j])
+			{
+				if (a[i + 1] >= 'a' && a[i + 1] <= 'z')
+					a[i + 1] = toupper(a[i + 1]);
+			}
+		}
 	}
 	return (a);
 }
