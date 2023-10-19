0ad4628600c4800426c2e83391fc6c857f8bef8f
HALLAL <hmdhallal@gmail.com>
2023-10-19 07:17:28 -0700

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * rot13 - function that encodes a string using rot13.
+ * @a: string should be encded
+ * Return: void
+ */
+char *rot13(char *a)
+{
+	int i, j;
+	char *p = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *q = "NOPQRSTUVWXYZABCDEFGHIJKLM";
+	char *k = "abcdefghijklmnopqrstuvwxyz";
+	char *l = "nopqrstuvwxyzabcdefghijklm";
+	char *b = a;
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		for (j = 0; j < 26; j++)
+		{
+			if (a[i] == p[j])
+			{
+				a[i] = q[j];
+				break;
+			}
+			if (a[i] == k[j])
+			{
+				a[i] = l[j];
+				break;
+			}
+		}
+	}
+	return (b);
+}
