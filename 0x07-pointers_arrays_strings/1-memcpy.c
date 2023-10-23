767568c0bb3bc167799fecf0812c0cb9b8aec216
HALLAL <hmdhallal@gmail.com>
2023-10-23 05:36:44 -0700

    0x07-pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _memcpy - function that copies memory area
+ * @dest: var 1
+ * @src: var 2
+ * @n: the size
+ * Return char
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	memcpy(dest, src, n);
+	return (dest);
+}
