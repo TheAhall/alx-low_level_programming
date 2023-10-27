532ea5e4a0ef9bb092f115afd2673c9a352066cb
HALLAL <hmdhallal@gmail.com>
2023-10-27 06:27:44 -0700

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entery point
+ * @argc: number of arg
+ * argv: vertor
+ * Return: Always 0
+ */
+int main(int argc, char* argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
