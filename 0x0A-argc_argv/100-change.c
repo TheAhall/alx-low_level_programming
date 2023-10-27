6af185e4d4b2bbf6248b994f7b5587817d11776d
HALLAL <hmdhallal@gmail.com>
2023-10-27 08:10:29 -0700

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entery point
+ * @argc: number of arg
+ * @argv: vertor
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int cent, coin = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	cent = atoi(argv[1]);
+	if (cent < 0)
+		printf("0\n");
+
+	while(cent > 0)
+	{
+		if (cent >= 25)
+			cent -= 25;
+		else if (cent >= 10)
+			cent -= 10;
+		else if (cent >= 5)
+			cent -= 5;
+		else if (cent >= 2)
+			cent -= 2;
+		else if (cent >= 1)
+			cent -= 1;
+		coin++;
+	}
+	printf("%d\n", coin);
+	return (0);
+}
