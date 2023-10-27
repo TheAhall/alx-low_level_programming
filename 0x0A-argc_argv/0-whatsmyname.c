5e0528caf79b51e42a5cacb347669346f023b15e
HALLAL <hmdhallal@gmail.com>
2023-10-27 06:15:13 -0700

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entery point
+ *
+ * Return: Always 0
+ */
+int main(int argc, char* argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		if (i == 0)
+			printf("%s\n", argv[i]);
+	}
+	return (0);
+}
