4afbfbfacf0811d16795944b1ca8d544ca4b7f1a
HALLAL <hmdhallal@gmail.com>
2023-10-27 06:19:13 -0700

    0x0A-argc_argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,10 +2,11 @@
 
 /**
  * main - entery point
- *
+ * @argc: number of arg
+ * @argv: vector
  * Return: Always 0
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 
