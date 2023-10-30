81289f62672a6f6a50b3c0bd3c141303353c19ab
HALLAL <hmdhallal@gmail.com>
2023-10-30 06:53:52 -0700

    0x0B-malloc_free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,8 +14,10 @@ char *str_concat(char *s1, char *s2)
 	char *s;
 	int i, a;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	j = strlen(s1);
 	k = strlen(s2);
 	a = j + k;
