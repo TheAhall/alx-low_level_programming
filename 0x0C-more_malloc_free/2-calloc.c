2f37584c66c320f905a68c1fdaada1d2e3e3512c
HALLAL <hmdhallal@gmail.com>
2023-11-02 08:19:07 -0700

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,15 +8,13 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int i;
 	char *s;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	s = malloc(nmemb * size + 1);
+	s = calloc(nmemb, size);
 	if (s == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
-		s[i] = 0;
-	return (s);
+	else
+		return (s);
 }
