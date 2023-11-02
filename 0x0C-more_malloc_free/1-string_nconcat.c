f21a4925806b2c9a55834b9e3eab6e2cd517974f
HALLAL <hmdhallal@gmail.com>
2023-11-02 07:38:01 -0700

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,6 +12,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, l, l1 = 0, l2 = 0;
 	char *s;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (i = 0; s1[i] != '\0'; i++)
 		l1++;
 	for (i = 0; s2[i] != '\0'; i++)
