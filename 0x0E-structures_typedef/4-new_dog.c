5c5161bc31151742910e1378cee328a8e9e5c1e0
HALLAL <hmdhallal@gmail.com>
2023-11-06 06:12:09 -0800

    0x0E-structures_typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -19,7 +19,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
-	(*d).name = malloc(sizeof(char) * ln + 1);
+	(*d).name = malloc(sizeof(char) * (ln + 1));
 	if ((*d).name == NULL)
 	{
 		free(d);
@@ -29,7 +29,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		(*d).name[i] = name[i];
 	(*d).name[i] = '\0';
 	(*d).age = age;
-	(*d).owner = malloc (sizeof(char) * lo + 1);
+	(*d).owner = malloc (sizeof(char) * (lo + 1));
 	if ((*d).owner == NULL)
 	{
 		free(d);
