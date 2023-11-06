0c5193671035eac9b1ca24081f83cc8e75b5feaa
HALLAL <hmdhallal@gmail.com>
2023-11-06 05:10:11 -0800

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * print_dog - function that prints a struct dog
+ * @d: the struct to print
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", (*d).name);
+		printf("Age: %f\n", (*d).age);
+		printf("owner: %s\n", (*d).owner);
+	}
+}
