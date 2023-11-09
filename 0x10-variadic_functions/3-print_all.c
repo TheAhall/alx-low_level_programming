0bc146816c37f803802044e9340f0a369551c157
HALLAL <hmdhallal@gmail.com>
2023-11-09 05:43:28 -0800

    0x10-variadic_functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,7 +2,7 @@
 
 /**
  * print_all - function that prints anything.
- * @forma: list of types of arguments passed to the function
+ * @format: list of types of arguments passed to the function
  * Return: void
  */
 void print_all(const char * const format, ...)
