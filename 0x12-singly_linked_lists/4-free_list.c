0a5c41d57c4cf18f45a6b408199dd09222bab0ac
HALLAL <hmdhallal@gmail.com>
2023-11-16 06:50:42 -0800

    0x12-singly_linked_lists

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_list - function that frees a list_t list
+ * @head: list should be freed
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *h;
+
+	for (i = 0; head != NULL; i++)
+	{
+		h = head
+		free(h->str);
+		head = head->next;
+		free(h);
+	}
+	free(head);
+}
