e2a5c8ef02ea4b3ec9df7d92b28a14c0353e6d65
HALLAL <hmdhallal@gmail.com>
2023-11-16 07:02:26 -0800

    0x12-singly_linked_lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * print_before_execute - function that prints before execute
+ */
+void print_before_execute(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
