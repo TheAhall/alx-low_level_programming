b42da3cf62019ad6a19309bd4dffdf3321a1df70
HALLAL <hmdhallal@gmail.com>
2023-11-21 04:19:14 -0800

    0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * listint_len - function that returns the number of elements
+ * @h: the list
+ * Return: number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t i;
+	
+	for (i = 0; h != NULL; i++)
+		h = h->next;
+	return (i);
+}
