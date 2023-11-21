ed4ef8227f43c8bb49c4cabca20811571372976c
HALLAL <hmdhallal@gmail.com>
2023-11-21 05:39:26 -0800

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,10 +11,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *h;
 	unsigned int i;
 
-	if (head == NULL)
-		return (NULL);
 	h = head;
 	for (i = 0; i < index; i++)
 		h = h->next;
-	return (h);
+	if (h == NULL)
+		return (NULL);
+	else
+		return (h);
 }
