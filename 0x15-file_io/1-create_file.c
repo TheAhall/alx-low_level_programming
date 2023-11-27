35cfa604705df2e6b0e1960cb4ca9df69a64bc19
HALLAL <hmdhallal@gmail.com>
2023-11-27 12:12:30 -0800

    0x15-file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	n = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (n == -1)
 		return (-1);
-	while (text_content[l] != '\0')
+	while (*text_content)
 		l++;
 	if (l != 0)
 		b = write(n, text_content, l);
