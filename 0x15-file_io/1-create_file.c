917be9a1125205048509fde4aa03e1b5f9345884
HALLAL <hmdhallal@gmail.com>
2023-11-27 12:09:51 -0800

    0x15-file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,11 +18,11 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	while (text_content[l] != '\0')
 		l++;
-	if (l != 0)
+	if (l)
 		b = write(n, text_content, l);
 	close(n);
 	if (b == l)
-		return (-1);
-	else
 		return (1);
+	else
+		return (-1);
 }
