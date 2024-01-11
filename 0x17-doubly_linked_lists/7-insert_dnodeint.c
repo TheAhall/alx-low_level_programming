fd6c37ce41c0e55f49dca04943afce6c88993576
HALLAL <hmdhallal@gmail.com>
2024-01-11 06:50:48 -0800

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,35 +12,30 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i;
 	dlistint_t *tmp1, *tmp2, *new;
 
-	if (h == NULL)
-		return (NULL);
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-	else
+	tmp1 = *h;
+	tmp2 = *h;
+	for (i = 0; i < idx - 1; i++)
 	{
-		tmp1 = *h;
-		tmp2 = *h;
-		for (i = 0; i < idx - 1; i++)
-		{
-			tmp1 = tmp1->next;
-			if (tmp1 == NULL)
-				return (NULL);
-		}
-		for (i = 0; i <= idx - 1; i++)
-		{
-			tmp2 = tmp2->next;
-			if (tmp2 == NULL)
-			{
-				return (add_dnodeint_end(h, n));
-			}
-		}
-		tmp1->next = new;
-		new->prev = tmp1;
-		new->next = tmp2;
-		return (*h);
+		tmp1 = tmp1->next;
+		if (tmp1 == NULL)
+			return (NULL);
 	}
+	for (i = 0; i <= idx - 1; i++)
+	{
+		tmp2 = tmp2->next;
+		if (tmp2 == NULL)
+		{
+			return (add_dnodeint_end(h, n));
+		}
+	}
+	tmp1->next = new;
+	new->prev = tmp1;
+	new->next = tmp2;
+	return (*h);
 }
