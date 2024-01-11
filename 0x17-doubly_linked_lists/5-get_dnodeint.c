b8a8550826841760bed40bf899e478e182c0c750
HALLAL <hmdhallal@gmail.com>
2024-01-11 05:39:27 -0800

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -11,6 +11,8 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	unsigned int i;
 	dlistint_t *tmp;
 
+	if (head == NULL)
+		return (NULL);
 	tmp = head;
 	for (i = 0; i < index; i++)
 		tmp = tmp->next;
