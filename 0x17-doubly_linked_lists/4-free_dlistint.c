7801e560a4d7a64ba00558bead2c05f7034afb23
HALLAL <hmdhallal@gmail.com>
2024-01-11 05:24:55 -0800

    0x17-doubly_linked_lists

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a dlistint_t list
+ * @head: the list
+ * Return: void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	if (head->next == NULL)
+		free(head);
+	else
+	{
+		while (head->next != NULL)
+		{
+			tmp = head;
+			head = head->next;
+			tmp->next = NULL;
+			head->prev = NULL;
+			free(tmp);
+		}
+		free(head);
+	}
+}
