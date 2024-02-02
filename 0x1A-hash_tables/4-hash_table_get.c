1cbfa623c9ece2c33397a62a39b3806a0ae43378
HALLAL <hmdhallal@gmail.com>
2024-02-02 06:09:48 -0800

    0x1A-hash_tables

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: the table
+ * @key: the key we are looking for
+ * Return: value associated with the element,
+ * or NULL if key couldn’t be foun.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *node;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
+	node = ht->array[index];
+	while(node != NULL && strcmp(node->key, key) != 0)
+		node = node->next;
+	if (node == NULL)
+		return (NULL);
+	return (node->value);
+}
