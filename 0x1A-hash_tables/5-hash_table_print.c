4b0a78f6b0535be94d179e949935929bed51216c
HALLAL <hmdhallal@gmail.com>
2024-02-02 06:38:38 -0800

    0x1A-hash_tables

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -14,22 +14,22 @@ void hash_table_print(const hash_table_t *ht)
 	if (ht == NULL)
 		return;
 	printf("{");
-		for (i = 0; i < ht->size; i++)
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
 		{
-			if (ht->array[i] != NULL)
+			if (c == 1)
+				printf(", ");
+			node = ht->array[i];
+			while (node != NULL)
 			{
-				if (c == 1)
-					printf(", ");
-				node = ht->array[i];
-				while (node != NULL)
-				{
-					printf("'%s': '%s'", node->key, node->value);
-					node = node->next;
-					if (node != NULL)
-						printf(",");
-				}
-				c = 1;
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
+				if (node != NULL)
+					printf(",");
 			}
+			c = 1;
 		}
-		printf("}\n");
+	}
+	printf("}\n");
 }
